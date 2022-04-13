d3c6730b02751d34be530945ef107711ebdfc0c7
Samantha Koh <sam@rocketacademy.co>
2022-04-13 17:23:18 +0800

    - Create templates for Batch page and tabs - Use HttpResponseRedirect instead of redirect for consistency

--- staff/urls.py
+++ staff/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import coding_basics_view, index, login_view
+from .views import batch_overview_view, batch_sections_view, batch_students_view, coding_basics_view, index, login_view
 
 urlpatterns = [
     path('', index, name='index'),
     path('login/', login_view, name='login_view'),
-    path('coding_basics/', coding_basics_view, name='coding_basics_view')
+    path('coding_basics/', coding_basics_view, name='coding_basics_view'),
+    # To-do: Update the following URLs when BatchViewSet is ready
+    path('batch/overview', batch_overview_view, name='batch_overview_view'),
+    path('batch/students', batch_students_view, name='batch_students_view'),
+    path('batch/sections', batch_sections_view, name='batch_sections_view')
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 
 from .forms import LoginForm
 
 def index(request):
-    return redirect(coding_basics_view)
+    return HttpResponseRedirect('/staff/coding_basics/')
 
 def login_view(request):
     if request.method == 'GET':
@@ -29,8 +29,28 @@ def login_view(request):
             return render(request, 'login.html', {'form': form})
 
         login(request, user)
-        return redirect(coding_basics_view)
+        return HttpResponseRedirect('/staff/coding_basics/')
 
+# To-do: Move to CourseViewSet when ready
 @login_required(login_url='/staff/login/')
 def coding_basics_view(request):
     return HttpResponse('Coding Basics!')
+
+# To-do: Move to BatchViewSet when ready
+@login_required(login_url='/staff/login/')
+def batch_overview_view(request):
+    return render(request,
+        'coding_basics/batch/overview.html',
+    )
+
+@login_required(login_url='/staff/login/')
+def batch_students_view(request):
+    return render(request,
+        'coding_basics/batch/students.html',
+    )
+
+@login_required(login_url='/staff/login/')
+def batch_sections_view(request):
+    return render(request,
+        'coding_basics/batch/sections.html',
+    )
