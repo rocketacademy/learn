0fe59c2d755310f15b4fe4e4c5bd3a18fa48515f
Samantha Koh <sam@rocketacademy.co>
2022-05-30 15:19:07 +0800

    Place script for stripe integration in separate .js file

---
+++ student/static/student/js/payment_preview.js
@@ -0,0 +1,16 @@
+$(function() {
+    const payable_type = document.getElementById("payable-type").value; 
+    const payable_id = document.getElementById("payable-id").value; 
+    fetch("/payment/config/")
+    .then((result) => { return result.json(); })
+    .then((data) => {
+        const stripe = Stripe(data.publicKey);
+        document.querySelector("#make-payment-button").addEventListener("click", () => {
+            fetch("/payment/create-checkout-session/" + payable_type + "/" + payable_id + "/")
+            .then((result) => { return result.json(); })
+            .then((data) => {
+                return stripe.redirectToCheckout({sessionId: data.sessionId})
+            })
+        });
+    });
+})
