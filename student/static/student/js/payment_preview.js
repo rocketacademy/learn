24efe191f68608ad8da0327df7987ddfdb003450
neokaiyuan <kai@rocketacademy.co>
2022-06-08 17:21:14 +0800

    Prettier linting

--- student/static/student/js/payment_preview.js
+++ student/static/student/js/payment_preview.js
@@ -1,11 +1,18 @@
 $(function () {
   const payable_type = document.getElementById("payable-type").value;
   const payable_id = document.getElementById("payable-id").value;
-  const payable_line_item_name = document.getElementById("payable-line-item-name").value;
-  const payable_line_item_amount_in_cents = document.getElementById("payable-line-item-amount-in-cents").value;
-  const payment_success_path = document.getElementById("payment-success-path").value;
-  const payment_cancel_path = document.getElementById("payment-cancel-path").value;
-
+  const payable_line_item_name = document.getElementById(
+    "payable-line-item-name"
+  ).value;
+  const payable_line_item_amount_in_cents = document.getElementById(
+    "payable-line-item-amount-in-cents"
+  ).value;
+  const payment_success_path = document.getElementById(
+    "payment-success-path"
+  ).value;
+  const payment_cancel_path = document.getElementById(
+    "payment-cancel-path"
+  ).value;
 
   fetch("/payment/stripe/config/")
     .then((result) => {
@@ -26,9 +33,10 @@ $(function () {
               payable_type: payable_type,
               payable_id: payable_id,
               payable_line_item_name: payable_line_item_name,
-              payable_line_item_amount_in_cents: payable_line_item_amount_in_cents,
+              payable_line_item_amount_in_cents:
+                payable_line_item_amount_in_cents,
               payment_success_path: payment_success_path,
-              payment_cancel_path: payment_cancel_path
+              payment_cancel_path: payment_cancel_path,
             }),
           })
             .then((result) => {
