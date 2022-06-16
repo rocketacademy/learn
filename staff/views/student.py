fb177cf04863506d39a76a89575a7bc59b2bd117
Samantha Koh <sam@rocketacademy.co>
2022-06-16 16:29:32 +0800

    - List students belonging to section in section detail page - Standardise naming of student variables across section detail and student list views and templates

--- staff/views/student.py
+++ staff/views/student.py
@@ -10,7 +10,7 @@ from student.models.enrolment import Enrolment
 
 class ListView(LoginRequiredMixin, View):
     def get(self, request, batch_id):
-        student_user_queryset = StudentUser.objects.filter(enrolment__in=Enrolment.objects.filter(batch_id=batch_id))
+        studentuser_queryset = StudentUser.objects.filter(enrolment__in=Enrolment.objects.filter(batch_id=batch_id))
         batch = Batch.objects.get(pk=batch_id)
 
         return render(
@@ -18,7 +18,7 @@ class ListView(LoginRequiredMixin, View):
             'basics/student/list.html',
             {
                 'batch': batch,
+                'students': studentuser_queryset,
                 'current_tab': 'students',
-                'student_user_queryset': student_user_queryset
             }
         )
