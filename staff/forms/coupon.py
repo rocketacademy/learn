a67640eafdc7e6762be89efd6901a67ac49b61b2
Samantha Koh <sam@rocketacademy.co>
2022-07-12 10:57:32 +0800

    Show errors on form if data provided by user is invalid

--- staff/forms/coupon.py
+++ staff/forms/coupon.py
@@ -21,3 +21,25 @@ class CouponForm(forms.ModelForm):
             'discount_amount': forms.NumberInput,
             'description': forms.Textarea(attrs={'rows': 2})
         }
+
+    def clean(self):
+        start_date = self.cleaned_data.get('start_date')
+        end_date = self.cleaned_data.get('end_date')
+
+        if start_date and end_date and start_date >= end_date:
+            message = forms.ValidationError(('Start date must be before end date'), code='invalid_date')
+            self.add_error('start_date', message)
+            self.add_error('end_date', message)
+
+        return self.cleaned_data
+
+    def clean_discount_amount(self):
+        discount_amount = self.cleaned_data.get('discount_amount')
+
+        if discount_amount < 1:
+            raise forms.ValidationError(
+                ('Discount amount should be greater than zero'),
+                code='invalid_discount_amount'
+            )
+
+        return discount_amount
