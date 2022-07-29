fcf35286adb3c0f7cb8d8ea998bcf3deefda465c
Nuuggs <bryluke000@gmail.com>
2022-07-29 17:53:57 +0800

    add validations for referral code on basics registration form - add tests for the above

--- student/forms/registration/student_info.py
+++ student/forms/registration/student_info.py
@@ -1,5 +1,7 @@
+from django.utils.timezone import now
 from django import forms
 
+from payment.models.coupon import Coupon
 from student.models.registration import Registration
 
 
@@ -58,3 +60,29 @@ class StudentInfoForm(forms.ModelForm):
         labels = {
             'referral_channel': 'How did you hear about us?'
         }
+
+    def clean_referral_code(self):
+        referral_code = self.cleaned_data.get('referral_code')
+        if referral_code is None:
+            return referral_code
+
+        coupons = Coupon.objects.filter(code=referral_code)
+        if not coupons:
+            raise forms.ValidationError(
+                ('Referral code is invalid'),
+                code='referral_code',
+            )
+
+        coupon = coupons.first()
+        if coupon.start_date and now() < coupon.start_date:
+            raise forms.ValidationError(
+                ('Referral code is not in effect yet'),
+                code='referral_code',
+            )
+        if coupon.end_date and now() > coupon.end_date:
+            raise forms.ValidationError(
+                ('Referral code has expired'),
+                code='referral_code',
+            )
+
+        return referral_code
