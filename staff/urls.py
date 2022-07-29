2408d0caf36717226b6a1308de5bbaeb601b6f43
Nuuggs <bryluke000@gmail.com>
2022-07-29 12:05:31 +0800

    create view to output csv file for zoom breakout rooms with all enrolled students emails and sections - add unit test coverage for the above

--- staff/urls.py
+++ staff/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('basics/batches/<int:batch_id>/', batch.DetailView.as_view(), name='batch_detail'),
     path('basics/batches/<int:batch_id>/edit/', batch.EditView.as_view(), name='batch_edit'),
     path('basics/batches/<int:batch_id>/enrolments/', enrolment.ListView.as_view(), name='enrolment_list'),
+    path('basics/batches/<int:batch_id>/enrolments/create-zoom-breakout-csv/', enrolment.create_zoom_breakout_csv, name='create_zoom_breakout_csv'),
     path('basics/batches/<int:batch_id>/sections/', section.ListView.as_view(), name='section_list'),
     path('basics/batches/<int:batch_id>/sections/<int:section_id>/', section.DetailView.as_view(), name='section_detail'),
     path('coupons/', coupon.ListView.as_view(), name='coupon_list'),
