33c389128150cbcaeea18b0473f8b7e08e4d6bbc
Samantha Koh <sam@rocketacademy.co>
2022-08-04 16:24:44 +0800

    Add validation and tests to CouponEffectForm

--- staff/forms/coupon_effect.py
+++ staff/forms/coupon_effect.py
@@ -13,3 +13,17 @@ class CouponEffectForm(forms.ModelForm):
             'discount_type': forms.Select(),
             'discount_amount': forms.NumberInput()
         }
+
+    def clean(self):
+        discount_amount = self.cleaned_data.get('discount_amount')
+        discount_type = self.cleaned_data.get('discount_type')
+
+        if discount_type == 'percent' and discount_amount > 100:
+            message = forms.ValidationError(
+                ('Discount should not be more than 100%'),
+                code='invalid_discount'
+            )
+            self.add_error('discount_amount', message)
+            self.add_error('discount_type', message)
+
+        return self.cleaned_data
