9636164e7f6bafc9d49d484f9065c5939c04a341
Samantha Koh <sam@rocketacademy.co>
2022-08-12 10:47:20 +0800

    Move discount amount to Discount model from StripeDiscount model

--- payment/models/discount.py
+++ payment/models/discount.py
@@ -3,6 +3,6 @@ from polymorphic.models import PolymorphicModel
 
 
 class Discount(PolymorphicModel):
-    original_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- payment/models/stripe_discount.py
+++ payment/models/stripe_discount.py
@@ -3,5 +3,4 @@ from payment.models.discount import Discount
 
 
 class StripeDiscount(Discount):
-    amount = models.DecimalField(max_digits=8, decimal_places=2)
     coupon_id = models.CharField(max_length=8)
