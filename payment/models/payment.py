ef723117c18a51ca144b0859315437646ad7b819
Samantha Koh <sam@rocketacademy.co>
2022-08-15 11:45:29 +0800

    Make Payment inherit from SafeDeleteModel

--- payment/models/payment.py
+++ payment/models/payment.py
@@ -1,8 +1,12 @@
 from django.db import models
 from polymorphic.models import PolymorphicModel
+from safedelete import SOFT_DELETE_CASCADE
+from safedelete.models import SafeDeleteModel
 
 
-class Payment(PolymorphicModel):
+class Payment(PolymorphicModel, SafeDeleteModel):
+    _safedelete_policy = SOFT_DELETE_CASCADE
+
     payable_id = models.IntegerField()
     payable_type = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
