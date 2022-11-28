2d9803e93bc7a3315b29c78daa58a5049bbd9053
Samantha Koh <sam@rocketacademy.co>
2022-11-28 18:43:45 +0800

    Fix bug to ensure that old Coding Basics batches can still be edited

--- staff/tests/views/basics/batch/test_basics_batch_edit.py
+++ staff/tests/views/basics/batch/test_basics_batch_edit.py
@@ -172,6 +172,79 @@ def test_valid_form_updates_and_creates_records(swe_fundamentals_batch, existing
     assert new_batch_schedule.start_time == new_batch_schedule_start_time
     assert new_batch_schedule.end_time == new_batch_schedule_end_time
 
+def test_valid_form_updates_and_creates_records_for_coding_basics(coding_basics_batch, existing_user, mocker):
+    new_start_date = coding_basics_batch.start_date + timedelta(1)
+    new_end_date = coding_basics_batch.end_date + timedelta(1)
+
+    new_sections_count = Section.objects.all().count() + 1
+    section = coding_basics_batch.section_set.first()
+    new_section_capacity = section.capacity + 1
+    new_price = coding_basics_batch.price + 1
+    new_type = Batch.FULL_TIME
+
+    new_batch_schedules_count = BatchSchedule.objects.all().count() + 1
+    new_batch_schedule_day = 'TUE'
+    new_batch_schedule_start_time = time(12, 0)
+    new_batch_schedule_end_time = time(14, 0)
+
+    payload = {
+        'start_date': new_start_date,
+        'end_date': new_end_date,
+        'sections': new_sections_count,
+        'capacity': new_section_capacity,
+        'price': new_price,
+        'type': new_type,
+        'batch-schedule-TOTAL_FORMS': [f"{new_batch_schedules_count}"],
+        'batch-schedule-INITIAL_FORMS': ['1'],
+        'batch-schedule-MIN_NUM_FORMS': ['0'],
+        'batch-schedule-MAX_NUM_FORMS': ['7'],
+        'batch-schedule-0-day': ['MON'],
+        'batch-schedule-0-start_time': ['10:00:00'],
+        'batch-schedule-0-end_time': ['18:00:00'],
+        'batch-schedule-1-day': [new_batch_schedule_day],
+        'batch-schedule-1-start_time': [new_batch_schedule_start_time],
+        'batch-schedule-1-end_time': [new_batch_schedule_end_time]
+    }
+    client.post('/staff/login/', {
+        'email': existing_user.email,
+        'password': settings.PLACEHOLDER_PASSWORD}
+    )
+
+    mocker.patch(
+        'student.library.slack.Slack.create_channel',
+        return_value='C1234567Q'
+    )
+
+    freezer = freeze_time(date.today())
+    freezer.start()
+    response = client.post(reverse('basics_batch_edit', kwargs={'batch_id': coding_basics_batch.id}), data=payload)
+    freezer.stop()
+
+    assert response.status_code == HttpResponseRedirect.status_code
+    assert response['Location'] == reverse('basics_batch_detail', kwargs={'batch_id': coding_basics_batch.id})
+
+    batch = Batch.objects.first()
+    assert batch.start_date == new_start_date
+    assert batch.end_date == new_end_date
+    assert batch.sections == new_sections_count
+    assert batch.capacity == new_sections_count * new_section_capacity
+    assert batch.price == new_price
+    assert batch.type == new_type
+
+    section_queryset = Section.objects.all()
+    first_section = section_queryset.first()
+    new_section = section_queryset.last()
+    assert first_section.capacity == new_section_capacity
+    assert section_queryset.count() == batch.sections
+    Slack.create_channel.assert_called_once_with(f"{batch.number}-{new_section.number}")
+
+    batchschedule_queryset = BatchSchedule.objects.all()
+    new_batch_schedule = batchschedule_queryset.last()
+    assert batchschedule_queryset.count() == new_batch_schedules_count
+    assert new_batch_schedule.day == new_batch_schedule_day
+    assert new_batch_schedule.start_time == new_batch_schedule_start_time
+    assert new_batch_schedule.end_time == new_batch_schedule_end_time
+
 def test_section_slack_channels_not_created_if_more_than_7_days_before_batch_starts(swe_fundamentals_batch, existing_user, mocker):
     swe_fundamentals_batch.start_date = date(2022, 1, 7)
     swe_fundamentals_batch.end_date = date(2022, 2, 1)
