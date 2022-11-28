81f0f94f57cd1dcddda84e04c102e99227feb926
Samantha Koh <sam@rocketacademy.co>
2022-11-28 18:23:54 +0800

    Update basics_batch_registration_list to cater for both SWE fundamentals batches and coding basics batches

--- staff/views/basics/basics_registration.py
+++ staff/views/basics/basics_registration.py
@@ -3,20 +3,18 @@ from django.shortcuts import render
 from django.views import View
 
 from staff.models import Batch
-from student.models.registration import Registration
 
 
 class ListView(LoginRequiredMixin, View):
     def get(self, request, batch_id):
-        registration_queryset = Registration.objects.filter(batch_id=batch_id)
-        batch = Batch.basics_objects.get(pk=batch_id)
+        batch = Batch.objects.get(pk=batch_id)
 
         return render(
             request,
             'basics/registration/list.html',
             {
                 'batch': batch,
-                'registrations': registration_queryset,
+                'registrations': batch.registration_set.all(),
                 'current_tab': 'registrations',
             }
         )
