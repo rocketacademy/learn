83836262527c3a876f75905c7a704abb7ab23ed5
Samantha Koh <sam@rocketacademy.co>
2022-11-29 00:01:14 +0800

    Update basics_enrolment_list views to cater for both SWE fundamentals and coding basics

--- staff/tests/views/basics/batch/test_basics_batch_enrolment_views.py
+++ staff/tests/views/basics/batch/test_basics_batch_enrolment_views.py
@@ -15,94 +15,87 @@ pytestmark = pytest.mark.django_db
 client = Client()
 
 
-def test_basics_batch_enrolment_list_anonymous_user_redirected_to_login(batch_factory):
-    batch = batch_factory()
-    request = RequestFactory().get(f"/basics/batches/{batch.id}/enrolments/")
+def test_swe_fundamentals_batch_enrolment_list_anonymous_user_redirected_to_login(swe_fundamentals_batch):
+    request = RequestFactory().get(f"/basics/batches/{swe_fundamentals_batch.id}/enrolments/")
     request.user = AnonymousUser()
 
     response = ListView.as_view()(request)
 
     assert response.status_code == HttpResponseRedirect.status_code
-    assert f"staff/login/?next=/basics/batches/{batch.id}/enrolments/" in response.url
+    assert f"staff/login/?next=/basics/batches/{swe_fundamentals_batch.id}/enrolments/" in response.url
 
-def test_basics_batch_enrolment_list_logged_in_user_can_access(batch_factory, existing_user):
-    batch = batch_factory()
+def test_swe_fundamentals_batch_enrolment_list_contains_enrolments(enrolment_factory, existing_user):
+    swe_fundamentals_enrolment = enrolment_factory(swe_fundamentals=True)
     client.post('/staff/login/', {'email': existing_user.email, 'password': settings.PLACEHOLDER_PASSWORD})
 
-    response = client.get(reverse('basics_batch_enrolment_list', kwargs={'batch_id': batch.id}))
+    response = client.get(reverse('basics_batch_enrolment_list', kwargs={'batch_id': swe_fundamentals_enrolment.batch.id}))
 
     assert response.status_code == HttpResponse.status_code
+    assert list(response.context['enrolments']) == [swe_fundamentals_enrolment]
     assert 'basics/enrolment/list.html' in (template.name for template in response.templates)
 
-def test_create_zoom_breakout_csv_outputs_csv_file(mocker, batch_factory):
-    batch = batch_factory()
+def test_basics_batch_enrolment_list_contains_enrolments(enrolment_factory, existing_user):
+    coding_basics_enrolment = enrolment_factory(coding_basics=True)
+    client.post('/staff/login/', {'email': existing_user.email, 'password': settings.PLACEHOLDER_PASSWORD})
+
+    response = client.get(reverse('basics_batch_enrolment_list', kwargs={'batch_id': coding_basics_enrolment.batch.id}))
+
+    assert response.status_code == HttpResponse.status_code
+    assert list(response.context['enrolments']) == [coding_basics_enrolment]
+    assert 'basics/enrolment/list.html' in (template.name for template in response.templates)
+
+def test_create_zoom_breakout_csv_outputs_csv_file_for_basics(mocker, coding_basics_batch):
     student_email = 'bryan@test.com'
     room_name = 'room1'
     assert_value = [[room_name, student_email]]
     mocker.patch('staff.views.basics.basics_enrolment.prepare_zoom_breakout_csv_data', return_value=assert_value)
-    request = RequestFactory().get(f"/basics/batches/{batch.id}/enrolments/")
+    request = RequestFactory().get(f"/basics/batches/{coding_basics_batch.id}/enrolments/")
 
-    response = create_zoom_breakout_csv(request, batch.id)
+    response = create_zoom_breakout_csv(request, coding_basics_batch.id)
 
     response_content = str(response.content)
     assert response.headers['Content-Type'] == 'text/csv'
-    assert response.headers['Content-Disposition'] == f"attachment; filename=\"{batch.number}-zoom-breakout.csv\""
+    assert response.headers['Content-Disposition'] == f"attachment; filename=\"{coding_basics_batch.number}-zoom-breakout.csv\""
     assert student_email in response_content and room_name in response_content
 
-def test_prepare_zoom_breakout_csv_data_returns_room_names_student_emails(batch_factory):
-    batch = batch_factory()
-    section = Section.objects.create(
-        batch=batch,
-        number=1,
-        capacity=1
+def test_create_zoom_breakout_csv_outputs_csv_file_for_swe_fundamentals(mocker, swe_fundamentals_batch):
+    student_email = 'name@example.com'
+    room_name = 'room1'
+    mocker.patch(
+        'staff.views.basics.basics_enrolment.prepare_zoom_breakout_csv_data',
+        return_value=[
+            [
+                room_name,
+                student_email
+            ]
+        ]
     )
-    registration = Registration.objects.create(
-        course=batch.course,
-        batch=batch,
-        first_name='FirstName',
-        last_name='LastName',
-        email='user@email.com',
-        country_of_residence='SG',
-        referral_channel='word_of_mouth'
-    )
-    student_user = StudentUser.objects.create(
-        email='user@email.com',
-        first_name='FirstName',
-        last_name='LastName',
-        password=settings.PLACEHOLDER_PASSWORD
-    )
-    first_enrolment = Enrolment.objects.create(
-        registration=registration,
-        batch=batch,
-        section=section,
-        student_user=student_user
-    )
-    second_registration = Registration.objects.create(
-        course=batch.course,
-        batch=batch,
-        first_name='FirstName',
-        last_name='LastName',
-        email='user2@email.com',
-        country_of_residence='SG',
-        referral_channel='word_of_mouth'
-    )
-    second_student_user = StudentUser.objects.create(
-        email='user2@email.com',
-        first_name='FirstName',
-        last_name='LastName',
-        password=settings.PLACEHOLDER_PASSWORD
-    )
-    second_enrolment = Enrolment.objects.create(
-        registration=second_registration,
-        batch=batch,
-        section=section,
-        student_user=second_student_user
-    )
+    request = RequestFactory().get(f"/basics/batches/{swe_fundamentals_batch.id}/enrolments/")
 
-    result = prepare_zoom_breakout_csv_data(batch.id)
+    response = create_zoom_breakout_csv(request, swe_fundamentals_batch.id)
+
+    response_content = str(response.content)
+    assert response.headers['Content-Type'] == 'text/csv'
+    assert response.headers['Content-Disposition'] == f"attachment; filename=\"{swe_fundamentals_batch.number}-zoom-breakout.csv\""
+    assert student_email in response_content and room_name in response_content
+
+
+def test_prepare_zoom_breakout_csv_data_returns_room_names_student_emails_for_basics(enrolment_factory):
+    coding_basics_enrolment = enrolment_factory(coding_basics=True)
+
+    result = prepare_zoom_breakout_csv_data(coding_basics_enrolment.batch.id)
 
     assert result == [
         ['Pre-assign Room Name', 'Email Address'],
-        [f"room{first_enrolment.section.number}", first_enrolment.student_user.email],
-        [f"room{second_enrolment.section.number}", second_enrolment.student_user.email]
+        [f"room{coding_basics_enrolment.section.number}", coding_basics_enrolment.student_user.email],
+    ]
+
+def test_prepare_zoom_breakout_csv_data_returns_room_names_student_emails_for_swe_fundamentals(enrolment_factory):
+    swe_fundamentals_enrolment = enrolment_factory(swe_fundamentals=True)
+
+    result = prepare_zoom_breakout_csv_data(swe_fundamentals_enrolment.batch.id)
+
+    assert result == [
+        ['Pre-assign Room Name', 'Email Address'],
+        [f"room{swe_fundamentals_enrolment.section.number}", swe_fundamentals_enrolment.student_user.email],
     ]
